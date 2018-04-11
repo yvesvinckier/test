e524daabf8405c39b8f73c9381df48ccaaaa8e91
Yves Vinckier <yves.vinckier@live.fr>
2018-04-11 12:42:22 +0200

    back

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -27,7 +27,7 @@ exports.replaceRouterComponent = ({ history }) => {
   const store = createStore(initialState)
 
   const ConnectedRouterWrapper = ({ children }) => (
-    <Provider store={store(initialState)}>
+    <Provider store={store}>
       <Router history={history}>{children}</Router>
     </Provider>
   )
