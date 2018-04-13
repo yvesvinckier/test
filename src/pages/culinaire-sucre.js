62814f14f3f898e0db680a4315d034bb2f2c5382
Yves Vinckier <yves.vinckier@live.fr>
2018-04-13 14:20:35 +0200

    category: {title: {eq: "Culinaire sucré"}}

--- src/pages/culinaire-sucre.js
+++ src/pages/culinaire-sucre.js
@@ -68,7 +68,7 @@ export const query = graphql`
     allContentfulGallery(
         filter: {
             node_locale: {eq: "fr-FR"},
-            category: {name: {eq: "Culinaire sucré"}}
+            category: {title: {eq: "Culinaire sucré"}}
         },
         limit: 1000, 
         sort: { fields: [date], order: DESC }) {
